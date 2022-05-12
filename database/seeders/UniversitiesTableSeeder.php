19e9763c593f4393f5bcf2b60e97dfe45d998667
Felipe Carneiro <felipesan08@hotmail.com>
2022-05-12 00:15:44 -0300

    University table seeder added

---
+++ database/seeders/UniversitiesTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Dashboard\University;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Http;
+
+class UniversitiesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $response = Http::get('http://universities.hipolabs.com/search?country=United+States');
+        $data = $response->collect();
+    
+        $counter = 1;
+        foreach($data as $uni){
+            if($counter > 100){
+                break;
+            }
+            
+            University::create([
+                'alpha_two_code' => $uni['alpha_two_code'],
+                'country' => $uni['country'],
+                'domains' => $uni['domains'],
+                'name' => $uni['name'],
+                'web_pages' => $uni['web_pages']
+            ]);
+            $counter++;
+        }
+    }
+}
