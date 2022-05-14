f1ea0b18c5f6cdbf4b806427ff6978dc2ab076ca
Felipe Carneiro <flipecar97@gmail.com>
2022-05-14 02:53:54 -0300

    Endpoints added
    Login user and fetching user with token

--- routes/api.php
+++ routes/api.php
@@ -1,23 +1,15 @@
 <?php
 
 use App\Http\Controllers\API\UniversityAPIController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\API\UserAPIController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::get('universities', [UniversityAPIController::class, 'getUniversities'])->name('api.universities.get');
 Route::get('university/{university}', [UniversityAPIController::class, 'getUniversity'])->name('api.university.get');
+
+Route::post('user', [UserAPIController::class, 'login'])->name('api.user.login');
+
+Route::middleware('auth:sanctum')->group(function (){ 
+    Route::get('user', [UserAPIController::class, 'user'])->name('api.user.get');
+});
